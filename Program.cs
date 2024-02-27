6cf4a62b4fc4835a7bc77f0751867b2e4d5898b2
ramelendezs <157499497+ramelendezs@users.noreply.github.com>
2024-02-27 10:04:28 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿class Program
+{
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("Mi segundo Programa");
+
+        Console.WriteLine("Ingrese su Nombre");
+        string sNombre = Console.ReadLine();
+
+
+        Console.WriteLine("Ingrese su edad");
+        string sEdad = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su carrera");
+        string sCarrera = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su carné");
+        string sCarné = Console.ReadLine();
+
+        Console.WriteLine("soy " + sNombre + "tengo " + sEdad + "años y estudio la carrera de" + sCarrera + "Mi numero de carné es" + sCarné);
+        Console.ReadKey();
+    }
+
+}
+
+//* segundo programa *//
+
+class Program2
+{
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("Operaciones aritméticas");
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
